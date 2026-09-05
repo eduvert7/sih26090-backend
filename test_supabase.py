d6893386789d496b36358e9be5309a6aa88f0b71
Akshita <akshitasharma4057@gmail.com>
2026-09-05 11:56:49 +0530

    Add Supabase setup and test

---
+++ test_supabase.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(url, key)
+
+data = supabase.table("products").insert({
+    "title": "Test Product",
+    "description": "Just checking the connection"
+}).execute()
+
+print(data)
